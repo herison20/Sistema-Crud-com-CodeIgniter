43ed7e9c24a23339293144ae82a1a0f513e8551a
herison <herison.assuncao@outlook.com>
2018-07-03 02:16:00 -0400

    Criação do arquivo Alunos_model.php

---
+++ application/models/Alunos_model.php
@@ -0,0 +1,46 @@
+<?php
+class alunos_model extends CI_Model{
+    public function inserir($aluno){
+        $this->db->insert("alunos", $aluno);
+    }
+    public function buscaCursos(){
+        return $this->db->get("cursos")->result_array();
+    }
+    public function buscaTodos(){
+        $this->db->select('id_aluno, a.nome nome_aluno, data_nascimento, logradouro, numero, bairro, cidade, estado, c.nome nome_curso');
+        $this->db->from('alunos a');
+        $this->db->join('cursos c', 'a.id_curso = c.id_curso');
+        $this->db->order_by("id_aluno", "asc");
+        return $query = $this->db->get()->result_array();
+    }
+
+    public function selecionarId($id = NULL)
+    {
+        if ($id != NULL):
+            //Verifica se a ID no banco de dados
+            $this->db->select('id_aluno, a.nome nome_aluno, data_nascimento, logradouro, numero, bairro, cidade, estado, cep, c.id_curso, c.nome nome_curso, a.data_criacao');
+            $this->db->from('alunos a');
+            $this->db->join('cursos c', 'a.id_curso = c.id_curso');
+            $this->db->where('id_aluno', $id);
+            //limita para apenas um regstro
+
+            //pega os produto
+            $query = $this->db->get();
+            //retornamos o produto
+            return $query->row();
+        endif;
+    }
+
+    public function atualizarAluno($dados=NULL, $id=NULL)
+    {
+        if ($dados != NULL && $id != NULL) {
+            $this->db->update('alunos', $dados, array('id_aluno' => $id));
+        }
+    }
+    function deletar($id) {
+        if(is_null($id))
+            return false;
+        $this->db->where('id_aluno', $id);
+        return $this->db->delete('alunos');
+    }
+}
