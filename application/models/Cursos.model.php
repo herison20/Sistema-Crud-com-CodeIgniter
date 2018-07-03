822bc7d97f0c32a670465696fed9041b73c07aee
herison <herison.assuncao@outlook.com>
2018-07-03 02:19:25 -0400

    Alteração de comentários.php

--- application/models/Cursos.model.php
+++ application/models/Cursos.model.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH')	OR exit('No	direct	script	access	allowed');
-class cursos_model extends CI_Model
+
+class Cursos_model extends CI_Model
 {
     public function inserir($curso)
     {
@@ -32,19 +33,15 @@ class cursos_model extends CI_Model
     public function selecionarId($id = NULL)
     {
         if ($id != NULL):
-            //Verifica se a ID no banco de dados
             $this->db->select('id_curso, c.nome nome_curso, c.data_criacao criacao_curso, p.id_professor, p.nome nome_professor');
             $this->db->from('cursos c');
             $this->db->join('professores p', 'c.id_professor = p.id_professor');
             $this->db->where('id_curso', $id);
-            //limita para apenas um regstro
-
-            //pega os produto
             $query = $this->db->get();
-            //retornamos o produto
             return $query->row();
         endif;
     }
+
     public function atualizarCurso($dados=NULL, $id=NULL){
         if($dados != NULL && $id != NULL){
             $this->db->update('cursos', $dados, array('id_curso'=>$id));
