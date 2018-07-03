5c1dca353e1ceb54a0214b33d6c322eb4b649fdf
herison <herison.assuncao@outlook.com>
2018-07-03 02:12:28 -0400

    Criação das View home

---
+++ application/views/home.php
@@ -0,0 +1,8 @@
+<?php $this->load->view('commons/header'); ?>
+<div class="container bloco-home">
+    <div class="inner cover">
+        <h1 class="titulo cover-heading">Bem Vindo ao Crud - Flex Peak</h1>
+        <p class="texto lead">Para iniciar os cadastros, basta acessar uma das opções de cadastro no menu acima. <br>Divirta-se! </p>
+    </div>
+</div>
+<?php $this->load->view('commons/footer'); ?>
