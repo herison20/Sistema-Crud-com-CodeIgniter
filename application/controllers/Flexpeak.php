2b420cc392b6b1e2e5893e6a451c8af02dfce4b0
herison <herison.assuncao@outlook.com>
2018-07-03 01:53:51 -0400

    Crição do Controller Flexpeak.php, Classe e Métodos

---
+++ application/controllers/Flexpeak.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hérison Assunção
+ * Date: 29/06/2018
+ */
+defined('BASEPATH')	OR exit('No	direct	script	access	allowed');
+
+class Flexpeak extends CI_Controller{
+
+    function index(){
+        $this->load->view('home');
+    }
+
+    // Método para chamar a view fpdf para gerar relatório PDF
+    public function fpdf(){
+        // Carregamos a library
+        $this->load->library('myfpdf');
+        $this->load->model("Fpdf_model");
+        $lista= $this->Fpdf_model->consultaBanco();
+        $dados = array("alunos"=>$lista);
+
+        // Carregamos a view
+        $this->load->view('fpdf', $dados);
+
+    }
+
+}
+?>
