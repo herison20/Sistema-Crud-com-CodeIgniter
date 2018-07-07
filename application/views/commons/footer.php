7d99accb47ebf45d5d85c9f95780239cb15d3d66
Hérison Assunção <herison.assuncao@outlook.com>
2018-07-07 13:52:24 -0400

    Retirando espaços

--- application/views/commons/footer.php
+++ application/views/commons/footer.php
@@ -1,14 +1,11 @@
 </main>
-
 <footer class="fixar-rodape">
     <div class="container">
-        <p class="text-muted">&copy;2017 - Hérison Assunção / Desenvolvedor Web</p>
+        <p class="text-muted">&copy;2018 - Hérison Assunção / Desenvolvedor Web</p>
     </div>
 </footer>
-
 <script src="<?= base_url('assets/js/jquery-3.2.1.min.js') ?>"></script>
 <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
 <script src="<?= base_url('assets/js/jquery.mask.min.js')?>"></script>
-
 </body>
 </html>
